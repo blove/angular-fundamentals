c2bd13f4b05648e7fad78e762b0653b6f7a31fc8
Brian Love <brian@brianflove.com>
2019-04-13 18:00:39 -0600

    Template Strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{ name }}</h1>
+    <p>Age: {{ age - 10 }} 😉</p>
     <p>Follow me: {{ twitter }}</p>
+    <p>
+      Enjoys knitting?
+      {{ likesKnitting ? 'sweeeet' : 'you're missing out' }}
+    </p>
   `
 })
 export class AppComponent {
   name = 'Brian Love';
+  age = 40;
   twitter = '@brian_love';
+  likesKnitting: boolean;
+
+  // add your favorite food
+
+  // add your favorite place to go on vacation
+
+  // add the name of your best friend
 }

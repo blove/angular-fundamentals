f94c5bce336ca43f17124cc0dada6d9007398dd2
Brian Love <brian@brianflove.com>
2019-04-14 09:22:56 -0600

    Use input binding and remove yarn.lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-box-of-donuts size="6"></app-box-of-donuts>
+    <app-box-of-donuts [size]="6"></app-box-of-donuts>
   `
 })
 export class AppComponent {}

1feda0ebd5956a49e870c790b80ab1316ce1d4b4
Brian Love <brian@brianflove.com>
2019-04-14 10:46:16 -0600

    Styling

--- src/app/box-of-donuts.component.ts
+++ src/app/box-of-donuts.component.ts
@@ -4,18 +4,43 @@ import { Donut } from './models/donut.interface';
 @Component({
   selector: 'app-box-of-donuts',
   template: `
-    <p>
+    <div class="heading">
       <span *ngIf="size < 5">Small</span><span *ngIf="size > 7">Large</span> Box
-    </p>
-    <p *ngIf="isFull()">Box full</p>
-    <app-donut *ngFor="let donut of donuts" [donut]="donut"></app-donut>
+      <p *ngIf="isFull()">Box full</p>
+    </div>
+    <div class="donuts">
+      <!-- Add property binding for the showName property -->
+      <app-donut *ngFor="let donut of donuts" [donut]="donut"></app-donut>
+    </div>
+    <!-- Add a button with a click event handler that will toggle the showName property -->
   `
 })
 export class BoxOfDonutsComponent {
   donuts: Donut[] = [
-    { name: 'Bacon glazed', icing: true },
+    {
+      name: 'Chocolate Frosted',
+      icing: true,
+      fileName: 'assets/img/donuts/donut1.png'
+    },
+    {
+      name: 'Strawberry Frosted',
+      icing: true,
+      fileName: 'assets/img/donuts/donut2.png'
+    },
+    {
+      name: 'Chocolate Dipped',
+      icing: true,
+      fileName: 'assets/img/donuts/donut3.png'
+    },
+    {
+      name: 'Cherry Dipped',
+      icing: true,
+      fileName: 'assets/img/donuts/donut4.png'
+    },
     { name: 'Sirachi Infused', icing: false }
   ];
+
+  // add a showName property that is a boolean value
 
   @Input() size = 6;
 

8dbafcbd4592da07c51af6f6cdb2a97409bf7a2c
Brian Love <brian@brianflove.com>
2019-04-18 17:25:46 -0600

    Custom Event Bindings

--- src/app/box-of-donuts.component.ts
+++ src/app/box-of-donuts.component.ts
@@ -5,78 +5,62 @@ import { Donut } from './models/donut.interface';
   selector: 'app-box-of-donuts',
   template: `
     <div class="heading">
-      <span *ngIf="size < 5">Small</span><span *ngIf="size > 7">Large</span> Box
-      <p *ngIf="isFull()">Box full</p>
+      <div>
+        <span *ngIf="size < 5">Small</span>
+        <span *ngIf="size > 7">Large</span>
+        Box
+      </div>
+      <div>
+        {{ donuts ? donuts.length : 0 }}/{{ size }}
+        <span *ngIf="isFull()">Box full</span>
+      </div>
     </div>
     <div class="donuts">
-      <!-- Add click event handler to set the selectedDonut -->
-      <!-- Add the 'selected' class to the app-donut component when it is the currently selected donut -->
-      <app-donut
-        *ngFor="let donut of donuts"
-        [donut]="donut"
-        [showName]="showName"
-      ></app-donut>
+      <ng-container *ngFor="let donut of donuts">
+        <app-donut [donut]="donut"></app-donut>
+        <!-- add button that emits the remove event -->
+      </ng-container>
     </div>
-    <button (click)="showName = !showName"></button>
   `,
   styles: [
     `
+      :host {
+        border: 4px solid #e91e63;
+      }
+
       .selected {
         background: #efefef;
       }
 
-      /**
-       * Copy styles for app-box-of-donuts from the root styles.css file.
-       */
+      .heading {
+        background-color: #f8bbd0;
+        padding: 10px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
+
+      .donuts {
+        padding: 10px;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: flex-start;
+        flex-wrap: wrap;
+      }
     `
   ]
 })
 export class BoxOfDonutsComponent {
-  donuts: Donut[] = [
-    {
-      name: 'Chocolate Frosted',
-      icing: true,
-      fileName: 'assets/img/donuts/donut1.png'
-    },
-    {
-      name: 'Strawberry Frosted',
-      icing: true,
-      fileName: 'assets/img/donuts/donut2.png'
-    },
-    {
-      name: 'Chocolate Dipped',
-      icing: true,
-      fileName: 'assets/img/donuts/donut3.png'
-    },
-    {
-      name: 'Cherry Dipped',
-      icing: true,
-      fileName: 'assets/img/donuts/donut4.png'
-    },
-    {
-      name: 'Strawberry Dipped',
-      icing: true,
-      fileName: 'assets/img/donuts/donut5.png'
-    },
-    {
-      name: 'Vanilla Dipped',
-      icing: true,
-      fileName: 'assets/img/donuts/donut6.png'
-    },
-    {
-      name: 'Cherry Coated',
-      icing: true,
-      fileName: 'assets/img/donuts/donut7.png'
-    }
-  ];
+  /** The donuts in the box. */
+  @Input() donuts: Donut[];
 
+  // add custom remove event emitter that emits a Donut object
+
+  /** The size of the box. This is the maximum number of donuts it can hold. */
   @Input() size = 6;
 
-  // add a selectedDonut property that is of type `Donut`
-
-  showName = true;
-
   isFull(): boolean {
-    return this.donuts.length === this.size;
+    return this.donuts && this.donuts.length === this.size;
   }
 }

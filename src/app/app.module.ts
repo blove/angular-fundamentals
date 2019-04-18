63ad3f7a64e05432bea20025a7cff2aee018996a
Brian Love <brian@brianflove.com>
2019-04-18 17:25:46 -0600

    Custom Event Bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BoxOfDonutsComponent } from './box-of-donuts.component';
+import { DonutWallComponent } from './donut-wall.component';
 import { DonutComponent } from './donut.component';
 
 @NgModule({
-  declarations: [AppComponent, DonutComponent, BoxOfDonutsComponent],
+  declarations: [
+    AppComponent,
+    DonutComponent,
+    BoxOfDonutsComponent,
+    DonutWallComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]

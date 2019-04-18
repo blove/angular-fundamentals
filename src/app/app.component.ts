3bec7297f677322bcccf1f01992fe7fedc564298
Brian Love <brian@brianflove.com>
2019-04-18 17:25:46 -0600

    Custom Event Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Donut } from './models/donut.interface';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-box-of-donuts [size]="6"></app-box-of-donuts>
+    <!-- add output binding and invoke the onSelect() method -->
+    <app-donut-wall></app-donut-wall>
+    <!-- add output binding and invoke the onRemove() method -->
+    <app-box-of-donuts [donuts]="donuts" [size]="6"></app-box-of-donuts>
   `,
   styleUrls: ['app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  /** The selected donuts in the box. */
+  donuts: Donut[] = [];
+
+  // declare onRemove method that accepts a donut and removes it from the array of selected donuts
+
+  // declare onSelect method that accepts a donut and appends it to the array of selected donuts
+}
---
+++ src/app/donut-wall.component.ts
@@ -0,0 +1,90 @@
+import { Component } from '@angular/core';
+import { Donut } from './models/donut.interface';
+
+@Component({
+  selector: 'app-donut-wall',
+  template: `
+    <div class="logo">
+      <img src="/assets/img/logo.png" />
+    </div>
+    <div class="wall">
+      <div class="donut" *ngFor="let donut of donuts">
+        <app-donut [donut]="donut"></app-donut>
+        <!-- add button that emits the select event -->
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .logo {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin-bottom: 10px;
+      }
+
+      .wall {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        margin: 20px 0;
+      }
+
+      .donut {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+      }
+
+      .donut > button {
+        background: transparent;
+        border: 1px solid #999;
+      }
+    `
+  ]
+})
+export class DonutWallComponent {
+  /** The available donuts on the wall. */
+  donuts: Donut[] = [
+    {
+      name: 'Chocolate Frosted',
+      icing: true,
+      fileName: 'assets/img/donuts/donut1.png'
+    },
+    {
+      name: 'Strawberry Frosted',
+      icing: true,
+      fileName: 'assets/img/donuts/donut2.png'
+    },
+    {
+      name: 'Chocolate Dipped',
+      icing: true,
+      fileName: 'assets/img/donuts/donut3.png'
+    },
+    {
+      name: 'Cherry Dipped',
+      icing: true,
+      fileName: 'assets/img/donuts/donut4.png'
+    },
+    {
+      name: 'Strawberry Dipped',
+      icing: true,
+      fileName: 'assets/img/donuts/donut5.png'
+    },
+    {
+      name: 'Vanilla Dipped',
+      icing: true,
+      fileName: 'assets/img/donuts/donut6.png'
+    },
+    {
+      name: 'Cherry Coated',
+      icing: true,
+      fileName: 'assets/img/donuts/donut7.png'
+    }
+  ];
+
+  // add custom event emitter `select` that emits when a donut has been added
+}

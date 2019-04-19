9d491325aac5631733466d88178a7f241936fc7d
Brian Love <brian@brianflove.com>
2019-04-18 18:23:06 -0600

    Component Lifecycle Hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+
 import { Donut } from './models/donut.interface';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!-- add output binding and invoke the addDonut() method -->
-    <app-donut-wall></app-donut-wall>
-    <!-- add output binding and invoke the removeDonuts() method -->
-    <app-box-of-donuts [donuts]="donuts" [size]="6"></app-box-of-donuts>
+    <app-donut-wall (select)="onSelect($event)"></app-donut-wall>
+    <app-box-of-donuts
+      [donuts]="donuts"
+      [size]="6"
+      (remove)="onRemove($event)"
+    ></app-box-of-donuts>
   `,
   styleUrls: ['app.component.css']
 })
@@ -15,7 +18,16 @@ export class AppComponent {
   /** The selected donuts in the box. */
   donuts: Donut[] = [];
 
-  // declare addDonut method that accepts a donut and appends it to the array of selected donuts
+  onRemove(donut: Donut): void {
+    const index = this.donuts.findIndex(d => d.name === donut.name);
+    if (index === -1) {
+      return;
+    }
 
-  // declare removeDonut method that accepts a donut and removes it from the array of selected donuts
+    this.donuts.splice(index, 1);
+  }
+
+  onSelect(donut: Donut): void {
+    this.donuts = [...this.donuts, donut];
+  }
 }

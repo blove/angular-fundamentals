c3b8b586a1725e484dd8cd8be0cb019968d77384
Brian Love <brian@brianflove.com>
2019-04-20 17:02:40 -0600

    Reactive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,33 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Donut } from './models/donut.interface';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!-- Add output binding for the edit custom event -->
-    <app-donut-wall (select)="onSelect($event)"></app-donut-wall>
-    <!-- Add input binding for name property -->
+    <app-donut-wall
+      (edit)="donut = $event"
+      (select)="onSelect($event)"
+    ></app-donut-wall>
     <app-box-of-donuts
       [donuts]="donuts"
+      [name]="name"
       [size]="6"
       (remove)="onRemove($event)"
     ></app-box-of-donuts>
 
     <div class="customer-form">
-      <!-- add input for the customer's name -->
+      <h2>Customer</h2>
+      <input [(ngModel)]="name" />
     </div>
 
-    <div class="donut-form">
+    <div class="donut-form" *ngIf="donut">
       <h2>Donut Kitchen</h2>
-      <!-- add form -->
-      <!-- add name input -->
-      <!-- add price input -->
+      <!-- Use reactive forms instead of template-driven forms -->
+      <form #donutForm="ngForm" (ngSubmit)="onSubmit(donutForm)">
+        <input name="name" [(ngModel)]="donut.name" required />
+        <input name="price" [(ngModel)]="donut.price" required />
+      </form>
     </div>
   `,
   styleUrls: ['app.component.css']
@@ -30,9 +36,16 @@ export class AppComponent {
   /** The selected donuts in the box. */
   donuts: Donut[] = [];
 
-  // add a donut property for the donut that is being edited
+  /** The donut that is in the kitched (edited). */
+  donut: Donut;
 
-  // add name property
+  // declare donutForm FormGroup instance
+
+  /** The customer's name. */
+  name: string;
+
+  // declare constructor function that requires the FormBuilder instance
+  // declare the donutForm using the FormBuilder.group instance method
 
   onRemove(donut: Donut): void {
     const index = this.donuts.findIndex(d => d.name === donut.name);
@@ -45,5 +58,11 @@ export class AppComponent {
 
   onSelect(donut: Donut): void {
     this.donuts = [...this.donuts, donut];
+  }
+
+  // remove form argument and reference donutForm FormGroup instance
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
+    console.log(form.valid);
   }
 }
---
+++ src/app/validators/donut-name.validator.ts
@@ -0,0 +1,4 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+// add the donutNameValidator factory function
+// hint: use the following regular expression to validate the name: /(coated|dipped|frosted)/i.test()

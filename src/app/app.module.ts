c36758302bc1cb36e4be6f1e91121135d281df8b
Brian Love <brian@brianflove.com>
2019-04-20 17:27:20 -0600

    Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BoxOfDonutsComponent } from './box-of-donuts.component';
@@ -13,7 +13,7 @@ import { DonutComponent } from './donut.component';
     BoxOfDonutsComponent,
     DonutWallComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })

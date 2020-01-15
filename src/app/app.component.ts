a9ede0fcc7e2cde45b605c97af9971160414c83c
Brian Love <brian@brianflove.com>
2020-01-15 15:00:34 -0700

    updated template and instructions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,16 @@ import { Component } from '@angular/core';
     <h1>{{ name }}</h1>
     <p>Age: {{ age - 10 }} 😉</p>
     <p>Follow me: {{ twitter }}</p>
-    <p>
-      Enjoys knitting?
-      {{ likesKnitting ? 'sweeeet' : 'you're missing out' }}
-    </p>
   `
 })
 export class AppComponent {
   name = 'Brian Love';
   age = 40;
   twitter = '@brian_love';
-  likesKnitting: boolean;
 
-  // add your favorite food
+  // 1. add your favorite food
 
-  // add your favorite place to go on vacation
+  // 2. add your favorite place to go on vacation
 
-  // add the name of your best friend
+  // 3. add the name of your best friend
 }
